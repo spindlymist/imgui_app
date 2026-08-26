2784013ab9eeb3e7f3493dd07b136beeac967511
spindlymist <ocrobin@gmail.com>
2026-08-26 16:35:17 -0700

    Fix texture format

--- src/textures.rs
+++ src/textures.rs
@@ -66,7 +66,7 @@ impl<'a> Textures<'a> {
             mip_level_count: 1,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            format: wgpu::TextureFormat::Rgba8Unorm,
             usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
             view_formats: &[],
         });
